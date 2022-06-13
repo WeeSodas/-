9ae186b550d44579d069c249c53cb388cfdc9023
WeeSodas <107421777+WeeSodas@users.noreply.github.com>
2022-06-13 23:34:30 +0800

    Add files via upload

---
+++ train_cnn.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from time import *
+
+
+# 数据集加载函数，指明数据集的位置并统一处理为imgheight*imgwidth的大小，同时设置batch
+def data_load(data_dir, test_data_dir, img_height, img_width, batch_size):
+    # 加载训练集
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        label_mode='categorical',
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
+    # 加载测试集
+    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        test_data_dir,
+        label_mode='categorical',
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
+    class_names = train_ds.class_names
+    # 返回处理之后的训练集、验证集和类名
+    return train_ds, val_ds, class_names
+
+
+# 构建CNN模型
+def model_load(IMG_SHAPE=(224, 224, 3), class_num=12):
+    # 搭建模型
+    model = tf.keras.models.Sequential([
+        # 对模型做归一化的处理，将0-255之间的数字统一处理到0到1之间
+        tf.keras.layers.experimental.preprocessing.Rescaling(1. / 255, input_shape=IMG_SHAPE),
+        # 卷积层，该卷积层的输出为32个通道，卷积核的大小是3*3，激活函数为relu
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+        # 添加池化层，池化的kernel大小是2*2
+        tf.keras.layers.MaxPooling2D(2, 2),
+        # Add another convolution
+        # 卷积层，输出为64个通道，卷积核大小为3*3，激活函数为relu
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        # 池化层，最大池化，对2*2的区域进行池化操作
+        tf.keras.layers.MaxPooling2D(2, 2),
+        # 将二维的输出转化为一维
+        tf.keras.layers.Flatten(),
+        # The same 128 dense layers, and 10 output layers as in the pre-convolution example:
+        tf.keras.layers.Dense(128, activation='relu'),
+        # 通过softmax函数将模型输出为类名长度的神经元上，激活函数采用softmax对应概率值
+        tf.keras.layers.Dense(class_num, activation='softmax')
+    ])
+    # 输出模型信息
+    model.summary()
+    # 指明模型的训练参数，优化器为sgd优化器，损失函数为交叉熵损失函数
+    model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+    # 返回模型
+    return model
+
+
+def train(epochs):
+    # 开始训练，记录开始时间
+    begin_time = time()
+    # 加载数据集
+    train_ds, val_ds, class_names = data_load("C:/Users/Administrator.DESKTOP-30KLKF0/Desktop/Tianchi/new_data/train",
+                                              "C:/Users/Administrator.DESKTOP-30KLKF0/Desktop/Tianchi/new_data/val", 224, 224, 16)
+    print(class_names)
+    # 加载模型
+    model = model_load(class_num=len(class_names))
+    # 指明训练的轮数epoch，开始训练
+    history = model.fit(train_ds, validation_data=val_ds, epochs=epochs)
+    #  保存模型
+    model.save("models/cnn_plant_disease.h5")
+    # 记录结束时间
+    end_time = time()
+    run_time = end_time - begin_time
+    print('该循环程序运行时间：', run_time, "s")  # 该循环程序运行时间： 1.4201874732
+
+
+if __name__ == '__main__':
+    train(epochs=64)
